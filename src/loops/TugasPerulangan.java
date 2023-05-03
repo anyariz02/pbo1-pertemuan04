26141f0d90ef40e7859f334e1ff73a66ecabdea0
anyariz02 <anyanime.id@gmail.com>
2023-05-03 15:49:05 +0800

    Tugas Perulangan

---
+++ src/loops/TugasPerulangan.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class TugasPerulangan {
+    public static void main(String[] args) {
+        int x;
+
+        System.out.println("Perkalian 5-100 Menggunakan Perulangan For");
+
+        for (x = 1; x<21; x++) {
+
+            System.out.println("Perkalian " + x + " x 5" + " = " + x*5 );
+
+        }
+    }
+}
